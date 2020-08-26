f84c18ef70ba416d149a3274dd14974c2ae85ab1
Vasil <vava@queue-it.com>
2020-08-26 13:18:58 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import  { Redirect } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/protected">Protected</Link>
+            </li>
+            <li>
+              <Link to="/cancel">Cancel</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/protected">
+            <Protected />
+          </Route>
+          <Route path="/cancel">
+            <Comprobante />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home(){
+  return (<div>Home.</div>)
+}
+
+function Protected(){
+  const [passedQueue, setPassedQueue] = useState(false);  
+  const [cancelled, setCancelled] = useState(false);
+
+  const handleQueueEventListener = (e)=>{
+    console.log("queue passed event")
+    setPassedQueue(true);
+  }
+
+  useEffect(()=>{
+    window.addEventListener("queuePassed", handleQueueEventListener);
+    return ()=>{
+      window.removeEventListener("queuePassed", handleQueueEventListener);
+    };
+  }, [])
+
+
+  useEffect(() => {
+    if(passedQueue){
+      console.warn("making api calls!");
+    }else{
+      //If the user hasn't gone through the queue when running this component, send him to the queue
+      console.log("validating user")      
+      window.QueueIt.validateUser();
+    }
+  }, [passedQueue])
+
+  if(cancelled){
+    console.log("redirecting to cancel");
+    return (
+      <Redirect to="/cancel"/>
+    )
+  }else{
+    if(!passedQueue){
+      return (<div>You'll be queued..</div>)
+    }else{
+      return (
+        <div>
+          This is my protected content<br/>
+          <button onClick={()=>setCancelled(true)}>Cancel</button>
+        </div>
+      )
+    }
+  }
+}
+
+function Comprobante(){
+  const [passedQueue, setPassedQueue] = useState(false);
+
+  const handleQueueEventListener = (e)=>{
+    console.log("cancel queue passed event");
+    setPassedQueue(true);
+  }
+
+  useEffect(()=>{
+    window.addEventListener("queuePassed", handleQueueEventListener);
+    return ()=>{
+      window.removeEventListener("queuePassed", handleQueueEventListener);
+    };
+  }, [])
+
+  useEffect(()=>{
+    console.log("cancel - validating user.")
+    window.QueueIt.validateUser();
+  }, [passedQueue]);
+
+  return (<div>
+    Sessions has been cancelled, you will start over from the end of the queue next time.
+  </div>)
 }
 
 export default App;
